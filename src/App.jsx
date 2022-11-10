7ce0d180f252061a590aac1627099dbc8361cdda
Mohammad Ridwanul Alam <myworld.shoyab@gmail.com>
2022-11-11 00:52:49 +0600

    test production

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,20 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="rid1.xyz" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="rid1.xyz" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Dev + App</h1>
+      <h1>PRODUCTION + App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>This is production mode</p>
+        <p>This is ========PRODUCTION======== mode</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
