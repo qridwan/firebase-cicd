480c05cf785c80ecbb011a0e9631e70e070fa98e
Mohammad Ridwanul Alam <myworld.shoyab@gmail.com>
2022-11-11 01:36:01 +0600

    add dev btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import WORK from "./assets/work.webp";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
       {/* < href="rid1.xyz" target="_blank"> */}
-        <img src={WORK} height="50%" width="80%" />
-      
+      <img src={WORK} height="50%" width="80%" />
+
       <div>
         <a href="rid1.xyz" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -20,10 +18,10 @@ function App() {
         </a>
       </div>
       <h1>PRODUCTION MODE</h1>
+      <a href="https://rid1-test-development.web.app">
+        <button>GO TO DEV-MODE</button>
+      </a>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>Stay calm site is live!</p>
       </div>
     </div>
